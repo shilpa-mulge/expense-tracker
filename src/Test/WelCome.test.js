08aebc1f0e7585a2d749cbe9e7a276324061ba60
shilpa <mulgeshilpa@gmail.com>
2023-03-26 21:25:51 +0530

    deleting unwanted

--- src/Test/WelCome.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import Welcome from "../MainNavigation/Welcom"
-import { BrowserRouter } from "react-router-dom"
-test('renders Welcome', async()=>{
-    render(<BrowserRouter><Welcome/></BrowserRouter>)
-    const LinkElement=screen.getByText('Welcome')
-    expect(LinkElement).toBeInTheDocument()
-})
